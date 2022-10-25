d5b477baa0fecaeb0ccc6346491b2f356e87fc47
Ilya Paymushkin <ipaymushkin76@gmail.com>
2022-10-25 23:03:38 +0300

    change api endpoint

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5007';
+const BASE_URL = 'http://192.168.1.40:5007';
+// const BASE_URL = 'http://localhost:5007';
 
 export const fetchInstance = axios.create({
   baseURL: BASE_URL,
